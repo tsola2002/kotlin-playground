a3d8877f56af4c21b3d7375edb0d094af91dc46f
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-10-17 20:43:46 +0100

    varables

---
+++ src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -0,0 +1,14 @@
+package com.kotlinplayground.basics
+
+fun main() {
+    val name : String = "Omatsola"
+    println(name)
+
+    // WILL NOT WORK
+    //name = "laju"
+
+    var age : Int = 34
+    println(age)
+    age = 40
+    println(age)
+}
